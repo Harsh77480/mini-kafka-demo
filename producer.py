c5470c11111c3af435baa5cbb3759a5d6292cf31
Harshwardhan Gupta <64406660+Harsh77480@users.noreply.github.com>
2026-02-19 09:44:04 +0530

    Configure producer with linger.ms and enhance comments
    Added linger.ms configuration to producer for batching messages and improved comments explaining producer behavior and flush method.

--- producer.py
+++ producer.py
@@ -6,7 +6,8 @@ import json
 import uuid
 from confluent_kafka import Producer
 
-producer = Producer({'bootstrap.servers': 'localhost:9092'})
+producer = Producer({'bootstrap.servers': 'localhost:9092', "linger.ms": 5}) 
+ # linger.ms : 5  means "Wait up to 5 milliseconds before actually sending the message over the network.". It acts like a bus waiting at a stop. Instead of sending 10 separate network requests for 10 wallet transactions that happened almost instantly, the producer waits 5 milliseconds, groups all 10 transactions into a single "batch", and sends them in one go.
 
 # event to be sent to kafka topic 'orders'
 order = {  
@@ -15,6 +16,7 @@ order = {
     'item' : 'Chicken Burger',
     'quantity': 2
 }
+
 
 # we have to convert the order dictionary to bit string before sending
 value = json.dumps(order).encode('utf-8')
@@ -33,5 +35,10 @@ def delivery_report(err, msg):
 producer.produce(topic='orders', value=value, callback=delivery_report)
 
 producer.flush()  
-# wait for any outstanding messages to be delivered and delivery reports to be received
-# ensures we dont have any messages that are not sent to kafka broker yet.
+#When you write producer.produce(topic, message) in your code, the message does not instantly go over the network. Instead, it is placed into a local memory buffer inside your Python application, and a background thread takes care of actually sending it to Kafka (respecting that linger.ms wait time).
+#This makes your application super fast because it doesn't have to pause and wait for the network every time it generates a transaction. But it introduces a risk: What if your application shuts down while messages are still sitting in that local memory buffer? They would be deleted and lost forever.
+#This is where producer.flush() comes in:
+#It acts as a roadblock: When you call producer.flush(), it blocks your current Python thread.
+#It empties the buffer: It forces the producer to instantly send any messages currently sitting in the local memory buffer to Kafka.
+#It waits for confirmation: It will pause your code until Kafka Broker replies with the acks (that broker has saved the messages) for all those remaining messages. 
+#On receieving acks(acknoledgement) only callback is called 
